6dba358a8bfbed237a5de20651faee9615172e00
Alejandro Echandia <alejandro.echandia@shopify.com>
2022-06-09 08:38:50 +0000

    update null config

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,8 +1,24 @@
 local null_ls = require("null-ls")
 
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        bufnr = bufnr,
+    })
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
-    if client.server_capabilities.document_formatting then
-        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                lsp_formatting(bufnr)
+            end,
+        })
     end
 end
 
