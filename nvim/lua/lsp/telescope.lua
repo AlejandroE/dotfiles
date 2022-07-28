1b75b5fed026f997ad56d30c3e727714521b9cf2
Alejandro Echandia <alejandro.echandia@shopify.com>
2022-07-28 10:33:07 +0000

    pick bufferes by recent

--- nvim/lua/lsp/telescope.lua
+++ nvim/lua/lsp/telescope.lua
@@ -6,6 +6,12 @@ _telescope.setup {
       treesitter = false
     },
     path_display={"truncate"}
+  },
+  pickers = {
+    buffers = {
+      ignore_current_buffer = true,
+      sort_mru = true,
+    },
   }
 }
 require('telescope').load_extension('neoclip')
