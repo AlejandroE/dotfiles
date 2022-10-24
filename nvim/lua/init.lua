5ce831d978beb586697ae640a7c55f01b6019ab0
Alejandro Etchandia <alejandro.echandia@shopify.com>
2022-10-24 15:25:22 +0200

    copy from 🌀

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -4,3 +4,18 @@ require "neoclip".setup()
 require "octo".setup()
 require "treesitter"
 require "lsp"
+
+
+-- copy over from the remote that can't be named
+vim.g.clipboard = {
+  name = 'pbpaste',
+  copy = {
+    ['+'] = 'pbcopy',
+    ['*'] = 'pbcopy',
+  },
+  paste = {
+    ['+'] = 'pbpaste',
+    ['*'] = 'pbpaste',
+  },
+  cache_enabled = 1,
+}
