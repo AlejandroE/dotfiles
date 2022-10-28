0c1d3f39dad99f61416ede853d4bc6693a0486c9
Alejandro Etchandia <alejandro.echandia@shopify.com>
2022-10-28 15:47:36 +0200

    cmp update

--- nvim/lua/lsp/cmp.lua
+++ nvim/lua/lsp/cmp.lua
@@ -78,9 +78,9 @@
   })
 
   -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-lspconfig = require'lspconfig'
-lspconfig.tsserver.setup{
+  lspconfig = require'lspconfig'
+  lspconfig.tsserver.setup{
     capabilities = capabilities
-}
+  }
