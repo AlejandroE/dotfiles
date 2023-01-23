f0b6471eadf4194f04b5900966431b0fb4981be6
Alejandro Etchandia <alejandro.echandia@shopify.com>
2023-01-23 14:12:45 +0100

    use eslint lang server 🏎️

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -3,6 +3,7 @@ require "lsp.ui"
 require "lsp.cmp"
 require "lsp.on_attach"
 require "lsp.null-ls"
+require "lsp.eslint"
 completionKinds = require "lsp.completion-kinds"
 completionKinds.setup()
 require("trouble").setup {}
